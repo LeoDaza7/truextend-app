4fa180e1aa87d127a82d1c4289eb0b457a74021c
Leonardo Daza <leo.daza.769@gmail.com>
2020-05-20 19:13:11 -0400

    app-bar: search box component added.

--- src/components/class/app-bar.jsx
+++ src/components/class/app-bar.jsx
@@ -3,20 +3,34 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 
 const AppBrand = lazy(()=>import('../function/app-brand'))
+const SearchBox = lazy(()=>import('../function/search-box'))
 const AppDrawer = lazy(()=>import('../function/app-drawer'))
 
 class NavBar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      isDrawerOpen: false
+      isDrawerOpen: false,
+      searchBoxValue: ''
     }
     this.handleDrawerToggle = this.handleDrawerToggle.bind(this)
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this)
+    this.handleSearchChange = this.handleSearchChange.bind(this)
   }
   handleDrawerToggle(){
     this.setState((state) => ({
       isDrawerOpen: !state.isDrawerOpen
     }))
+  }
+  handleSearchSubmit (event) {
+    event.preventDefault()
+    alert('TODO: search method - Value: ' + this.state.searchBoxValue)
+    console.log(event.target.elements.searchInputId.value)
+  }
+  handleSearchChange (event) {
+    this.setState({
+      searchBoxValue: event.target.value
+    })
   }
   render() {
     return (
@@ -27,7 +41,12 @@ class NavBar extends Component {
           elevation={ 0 }
           className={ this.props.className }>
           <Toolbar>
-              <AppBrand toggleDrawer={ this.handleDrawerToggle }/>
+            <AppBrand toggleDrawer={ this.handleDrawerToggle }/>
+            <SearchBox
+              searchValue={ this.state.searchBoxValue }
+              submitHandler={ this.handleSearchSubmit }
+              changeHandler={ this.handleSearchChange }
+            />
           </Toolbar>
         </AppBar>
         <AppDrawer isDrawerOpen={ this.state.isDrawerOpen }/>
---
+++ src/components/function/search-box.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import InputBase from '@material-ui/core/InputBase'
+import IconButton from '@material-ui/core/IconButton'
+import Paper from '@material-ui/core/Paper'
+import SearchIcon from '@material-ui/icons/Search'
+
+export default function SearchBox(props) {
+  return (
+    <Paper
+      component='form'
+      className='mr-auto'
+      variant='outlined'
+      onSubmit={ props.submitHandler }>
+      <IconButton type='submit' className=''>
+        <SearchIcon />
+      </IconButton>
+      <InputBase
+        id='searchInputId'
+        name='searchInputName'
+        className='searchInputClassName'
+        placeholder='Search..'
+        type='text'
+        value={ props.searchValue }
+        onChange={ props.changeHandler }/>
+    </Paper>
+  )
+}
