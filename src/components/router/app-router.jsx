c614764a3f9168c394a10cf41260be4bd865ba95
Leonardo Daza <leo.daza.769@gmail.com>
2020-05-20 20:36:37 -0400

    users-page: Users component added.

--- src/components/router/app-router.jsx
+++ src/components/router/app-router.jsx
@@ -2,12 +2,14 @@ import React, { lazy } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const Home = lazy(()=>import('../class/home'))
+const Users = lazy(()=>import('../class/users'))
 
 export default function AppRouter() {
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/users' component={Users}/>
       </Switch>
     </Router>
   )
