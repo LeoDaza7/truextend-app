2e2b1cd85f275278dc6d9e0695bb4f504494c506
Leonardo Daza <leo.daza.769@gmail.com>
2020-05-20 22:35:41 -0400

    users-page: Users component updated.
    A basic example about fetching data from the GitHub API, and rendering it like a list.

--- src/components/class/users.jsx
+++ src/components/class/users.jsx
@@ -1,9 +1,54 @@
 import React, { Component } from 'react'
 
 export default class Users extends Component {
-  render() {
-    return (
-      <h2>Users Page</h2>
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: [],
+      isLoaded: false,
+      error: null
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.github.com/users?page=1&per_page=100').then(
+      response => response.json()
+    ).then(
+      result => {
+        this.setState({
+          isLoaded: true,
+          users: result
+        })
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      }
     )
+  }
+
+  render() {
+    const { error, isLoaded, users } = this.state
+    if (error) {
+      return <>Error: { error.message }</>
+    } else if (!isLoaded){
+      return <>Loading..</>
+    } else {
+      return (
+        <>
+          <h2>Users Page</h2>
+          <ul>
+            {users.map(user => (
+              <li key={user.id}>
+                {user.login}
+              </li>
+            ))}
+          </ul>
+        </>
+      )
+    }  
   }
 }
